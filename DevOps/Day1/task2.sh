c656c5edcf4c634af0dc88653e605ef21c6793bb
kjvjobin <76947418+kjvjobin@users.noreply.github.com>
2021-01-12 10:03:00 +0530

    Create task2.sh

---
+++ DevOps/Day1/task2.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+echo  "Reversed string:";
+echo "$1\n $2\n $3\n $4\n $5\n"| rev;
+
+echo "Date: "
+date +"%d/%m/%Y"
+echo "Time: "
+date +"%r"
+echo "Username:"
+whoami
+echo "Print working directory:"
+pwd
