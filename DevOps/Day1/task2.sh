3952ebe3bea37abb4ac8306787e8157557f40709
Jobin <76947418+kjvjobin@users.noreply.github.com>
2021-01-13 12:38:09 +0530

    Update task2.sh

--- DevOps/Day1/task2.sh
+++ DevOps/Day1/task2.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
 
-echo  "Reversed string:";
+echo "Input string:" "$1 $2 $3 $4 $5"
+echo "Reversed string:";
 echo "$1\n $2\n $3\n $4\n $5\n"| rev;
 
-echo "Date: "
-date +"%d/%m/%Y"
-echo "Time: "
-date +"%r"
-echo "Username:"
-whoami
-echo "Print working directory:"
-pwd
+#Variabbles
+curr_date=$(date +"%d/%m/%Y")
+curr_time=$(date +"%r")
+user=$(whoami)
+present_working_dir=$(pwd)
+
+#Fetching required using above variables
+echo "Date:" $curr_date
+echo "Time:" $curr_time
+echo "Username:" $user
+echo "Print working directory:" $present_working_dir
