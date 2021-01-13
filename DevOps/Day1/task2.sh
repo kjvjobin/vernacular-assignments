2cddad8b9a10520eb0db17d3a7fd01755b98b432
Jobin <76947418+kjvjobin@users.noreply.github.com>
2021-01-13 12:39:30 +0530

    Update task2.sh

--- DevOps/Day1/task2.sh
+++ DevOps/Day1/task2.sh
@@ -2,7 +2,7 @@
 
 echo "Input string:" "$1 $2 $3 $4 $5"
 echo "Reversed string:";
-echo "$1\n $2\n $3\n $4\n $5\n"| rev;
+echo "$1\n $2\n $3\n $4\n $5\n"| rev;  #reversing-parameters-logic
 
 #Variabbles
 curr_date=$(date +"%d/%m/%Y")
