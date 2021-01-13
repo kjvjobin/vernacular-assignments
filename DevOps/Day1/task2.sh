4051fa0550d280fb4ef0ed390131c77b6b32d122
Jobin <76947418+kjvjobin@users.noreply.github.com>
2021-01-13 12:47:00 +0530

    Update task2.sh

--- DevOps/Day1/task2.sh
+++ DevOps/Day1/task2.sh
@@ -5,13 +5,13 @@ echo "Reversed string:";
 echo "$1\n $2\n $3\n $4\n $5\n"| rev;  #reversing-parameters-logic
 
 #Variabbles
-curr_date=$(date +"%d/%m/%Y")
-curr_time=$(date +"%r")
+current_date=$(date +"%d/%m/%Y")
+current_time=$(date +"%r")
 user=$(whoami)
 present_working_dir=$(pwd)
 
 #Fetching required using above variables
-echo "Date:" $curr_date
-echo "Time:" $curr_time
+echo "Date:" $current_date
+echo "Time:" $current_time
 echo "Username:" $user
 echo "Print working directory:" $present_working_dir
